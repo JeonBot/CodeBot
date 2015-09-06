2d385f979afdb4e52c45acf3e3b0d209c701151e
JeonBot <wjswnsgh73@mail.hongik.ac.kr>
2015-09-06 18:43:35 +0900

    Java1

---
+++ MyInfoApp.java
@@ -0,0 +1,12 @@
+// <프로그램 번호: 2번 1장 Open Challenge(48쪽)>
+// 작성자: 전 준호(B289086)
+// 날짜  : 2015년 9월 3일
+// 설명  : 이 프로그램은 책 48쪽에 지시한 대로 자신을 소개하는 프로그램이다.
+
+public class MyInfoApp {
+        public static void main(String[] args) {
+                System.out.println("Junho Jeon");                        //출력과 개행을 위해 println을 사용하고 문자열을 입력
+                System.out.println("25 years old");
+                System.out.println("Computer Information Communication");
+        }
+}
---
+++ One2Nine.java
@@ -0,0 +1,14 @@
+// <프로그램 번호: 1번 1장 실습문제 3(51쪽)>
+// 작성자: 전 준호(B289086)
+// 날짜  : 2015년 9월 3일
+// 설명  : 이 프로그램은 1부터 9까지 출력하는 프로그램이다.
+
+public class One2Nine {
+        public static void main(String[] args) {
+                int i=1;
+                while(i<10) {
+                        System.out.print(i+" ");
+                        i++;
+                }
+        }
+}
